da95d917169c498b6914670ef56cbb6a515ff7d9
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2020-12-22 21:09:49 +0900

    Add files via upload

--- models/index.js
+++ models/index.js
@@ -1,21 +1,30 @@
-'use strict';
+//'use strict';
 
-const fs = require('fs');
-const path = require('path');
+//const fs = require('fs');
+//const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+//const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config')[env];
+
+
+const User=require('./user');
+const Post=require('./post');
+const Hashtag=require('./hashtag');
 const db = {};
 
-let sequelize;
+/*let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+}*/
 
-fs
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+
+/*fs
   .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
@@ -29,9 +38,20 @@ Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
-});
+});*/
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.User=User;
+db.Post=Post;
+db.Hashtag=Hashtag;
+
+User.init(sequelize);
+Post.init(sequelize);
+Hashtag.init(sequelize);
+
+User.associate(db);
+Post.associate(db);
+Hashtag.associate(db);
+//db.Sequelize = Sequelize;
 
 module.exports = db;
